28a9ce2cb802d8cad0473a4c4a173e27427e6e67
sujin-create <qsoojinq@naver.com>
2023-04-28 13:39:05 +0900

    Test : memberRepository testcode 추가

--- src/test/java/whereQR/project/Repository/MemberRepositoryTest.java
+++ src/test/java/whereQR/project/Repository/MemberRepositoryTest.java
@@ -1,14 +1,19 @@
 package whereQR.project.Repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+import whereQR.project.entity.member.Address;
 import whereQR.project.entity.member.Member;
+import whereQR.project.entity.member.PhoneNumber;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
@@ -20,7 +25,6 @@ class MemberRepositoryTest {
     EntityManager em;
 
     @Test
-    @Commit
     public void insertMemberTest() {
 
         //given
@@ -32,6 +36,47 @@ class MemberRepositoryTest {
 
         //then
         Assertions.assertThat(member.getEmail()).isEqualTo("email");
+    }
+
+    @Test
+    @DisplayName("find all Member")
+    public void findAllMember(){
+
+        //given
+        Member member1 = new Member("user1","user1@email.com",10);
+        Member member2 = new Member("user2","user2@email.com",20);
+
+        em.persist(member1);
+        em.persist(member2);
+
+        //when
+        List<Member> members =  memberRepository.findAll();
+
+        //then
+        Assertions.assertThat(members.size()).isEqualTo(2);
+
+    }
+
+    @Test
+    @DisplayName("updateMember")
+    @Commit
+    public void updateMember(){
+
+        //given
+        Member member1 = new Member("user1","user1@email.com",10);
+        em.persist(member1);
+
+        //when
+        Long memberId = member1.getId();
+        Member findMember = memberRepository.findById(memberId).orElseThrow();
+        Address address = new Address("city","1","11");
+        PhoneNumber phoneNumber = new PhoneNumber("82","010-8750-9331");
+        findMember.updateMember("user2",10,address ,phoneNumber);
+
+        //then
+        Assertions.assertThat(member1.getUsername()).isEqualTo("user2");
+        Assertions.assertThat(member1.getPhoneNumber()).isEqualTo(phoneNumber);
+
     }
 
 }
