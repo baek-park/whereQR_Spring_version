0ba13500706bfc8cc67eb798b1ce9d30d90c7ceb
sujin-create <qsoojinq@naver.com>
2023-04-30 01:55:43 +0900

    Refactor : Dto Getter Setter를 Data annotation으로 변경

--- src/main/java/whereQR/project/entity/dto/MemberDto.java
+++ src/main/java/whereQR/project/entity/dto/MemberDto.java
@@ -4,46 +4,13 @@ import lombok.Data;
 import org.apache.tomcat.jni.Address;
 import whereQR.project.entity.member.PhoneNumber;
 
+@Data
 public class MemberDto {
 
     private String username;
     private int age;
     private Address address;
     private PhoneNumber phoneNumber;
-
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public PhoneNumber getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(PhoneNumber phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
 
     public void MemberDto(String username, int age, Address address , PhoneNumber phoneNumber){
         this.username = username;
