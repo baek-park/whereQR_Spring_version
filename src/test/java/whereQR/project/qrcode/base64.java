b9d092819946664182e2737634e5e43b4ca225b5
sujin-create <qsoojinq@naver.com>
2023-04-30 00:32:29 +0900

    Test : base64 encoding/ decoding test

--- src/test/java/whereQR/project/qrcode/base64.java
+++ src/test/java/whereQR/project/qrcode/base64.java
@@ -1,2 +1,37 @@
-package whereQR.project.qrcode;public class base64 {
+package whereQR.project.qrcode;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+
+public class base64 {
+
+    private static String inputFilePath = "test.png";
+    private static String outputFilePath = "test.png";
+
+    @Test
+    public void fileToBase64StringConversion() throws IOException {
+
+        //given
+        ClassLoader classLoader = getClass().getClassLoader();
+        File inputFile = new File(classLoader.getResource(inputFilePath).getFile());
+
+        //image to byte -> encoding
+        byte[] fileContent = FileUtils.readFileToByteArray(inputFile);
+        String encodedString = Base64.getEncoder().encodeToString(fileContent);
+
+        //when
+        //encoded string to byte -> decoding
+        File outputFile = inputFile;
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        FileUtils.writeByteArrayToFile(outputFile, decodedBytes);
+
+        //then
+        Assertions.assertTrue(FileUtils.contentEquals(inputFile, outputFile));
+
+    }
 }
