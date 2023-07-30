e17d07514ecf551a9160415d1c5ca538fee45f47
baeksoojin <qsoojinq@naver.com>
2023-07-30 13:28:14 +0900

    fix : refactoring 이후 기존 test code 삭제

--- src/test/java/whereQR/project/ProjectApplicationTests.java
+++ src/test/java/whereQR/project/ProjectApplicationTests.java
@@ -1,13 +1,7 @@
 package whereQR.project;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
